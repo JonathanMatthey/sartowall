e895f9bdb178004e06bceb23e7622ebd33505613
manymengofishing <mattheyj@googlemail.com>
2012-10-02 23:39:07 -0400

    added colors for all images ! batch job done

--- batch-process-palette.js
+++ batch-process-palette.js
@@ -5,7 +5,9 @@ var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
 var nodeio = require('node.io');
-var options = {timeout: 10};
+var options = {timeout: 200}
+  , http = require('http')
+  , url = require('url');
 
 var cursor;
 
@@ -15,12 +17,19 @@ var palette = require('palette')
   , canvas = new Canvas
   , ctx = canvas.getContext('2d');
 
+var outCanvas = new Canvas(1000, 750);
+var ctx = outCanvas.getContext('2d');
+
 var mydb;
+
+var self;
+
+var photoCollection;
 
 exports.job = new nodeio.Job(options, {
   input: ['hello'],
   run: function (keyword) {
-    var self = this, results;
+    self = this;
 
     // connect to DB
     mydb = new Db('node-mongo-blog', new Server('localhost', 27017, {auto_reconnect: true}, {}));
@@ -28,8 +37,8 @@ exports.job = new nodeio.Job(options, {
       console.log('open');
 
       mydb.collection('photos', function(error, photo_collection){
-
-        cursor = photo_collection.find({}, {}).limit(3);
+        photoCollection = photo_collection;
+        cursor = photo_collection.find({});
 
         processNextPhoto();
       });
@@ -43,47 +52,75 @@ function processNextPhoto(){
     if(err) throw err;
 
     if(photo !== null){
-      console.log ('paletting this img' + photo.src);
+      console.log ('paletting this img ' + photo.src);
       paletteImg(photo);
     }
     else {
       mydb.close();
-      self.emit('done!2');
+      self.emit('JOB DONE - GO HOME !')
     }
   });
 }
 
 function paletteImg(photo){
 
-  var img = new Image;
-  console.log('in paletteImg');
-  img.onload = function(){
-    canvas.width = img.width;
-    canvas.height = img.height + 50;
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0);
-    console.log('in img.onload');
+  var regexGroups = photo.src.match(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/);
 
-    var x = 0;
-    var colors = palette(canvas, n);
-    colors.forEach(function(color){
-      var r = color[0]
-        , g = color[1]
-        , b = color[2]
-        , val = r << 16 | g << 8 | b
-        , str = '#' + val.toString(16);
-        console.log("color:" + str);
-      // ctx.fillStyle = str;
-      // ctx.fillRect(x += 31, canvas.height - 40, 30, 30);
-    });
-    processNextPhoto();
-    console.log('processNextPhoto(cursor)');
-    // save();
-  };
+  var host = regexGroups[3]; 
+  var path = regexGroups[4] + regexGroups[6];
 
-  img.src = photo.src;
-  console.log(' waiting to load ' + photo.src ) ;
+  http.get(
+    {
+        host: host,
+        port: 80,
+        path: path
+    },
+    function(res) {
+      var data = new Buffer(parseInt(res.headers['content-length'],10));
+      var pos = 0;
+      res.on('data', function(chunk) {
+        chunk.copy(data, pos);
+        pos += chunk.length;
+      });
+      res.on('end', function () {
+        img = new Canvas.Image;
+        img.src = data;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        // var x = 0;
+        var colorsArray = [];
+        var colors = palette(outCanvas, 5);
+        colors.forEach(function(color){
+          var r = color[0]
+            , g = color[1]
+            , b = color[2]
+            , val = r << 16 | g << 8 | b
+            , str = '#' + val.toString(16);
+            colorsArray.push(str);
+        });
+
+        photo.colors = colorsArray;
+
+        console.log(photo);
+
+        // Update the document using an upsert operation, ensuring creation if it does not exist
+        photoCollection.update({_id:photo._id}, photo, {upsert:true, safe:true}, function(err, result) {
+
+          console.log('update complete');
+          processNextPhoto();
+
+        });
+
+        // var out = fs.createWriteStream(__dirname + '/my-out.png')
+        //   , stream = outCanvas.createPNGStream();
+
+        // stream.on('data', function(chunk){
+        //   out.write(chunk);
+        // });
+      });
+    }
+);
+
+
 }
 
 function savePhotos(photos, callback) {
