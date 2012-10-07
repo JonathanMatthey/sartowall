714d02fa3eade9eafeb891e95aceb6c4e72cf6a6
manymengofishing <mattheyj@googlemail.com>
2012-10-07 15:20:29 -0400

    requier mongo

--- blog-scraper.js
+++ blog-scraper.js
@@ -1,8 +1,14 @@
+// heroku connect
+var mongo = require('mongodb');
+var database = null;
+
+// local connect
 var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
+
 var moment = require('moment');
 
 var nodeio = require('node.io');
